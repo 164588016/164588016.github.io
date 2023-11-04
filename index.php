749e4592f83717f62802092cb2dab414a8325d19
164588016 <164588016@qq.com>
2023-11-05 07:20:36 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  echo '<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <title>PHP Example</title>
+  </head>
+  <body>
+    <h1>Hello everybody</h1>
+    <h1>This is a server</h1>
+    <h1>My Mailbox is 164588016@qq.com</h1>
+    <h1>bye,bye</h1>
+  </body>
+  </html>';
+?>
